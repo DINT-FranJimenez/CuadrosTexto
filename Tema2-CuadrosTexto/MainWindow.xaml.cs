05bcd61b388c772a053e3aad2b201db3fbee352e
FRANCISCO JIMÉNEZ LOZANO <frajimloz@alu.edu.gva.es>
2023-10-02 20:30:26 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-CuadrosTexto/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CuadrosTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void NombretextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+                if (nombreClienteLabel.Visibility == Visibility.Visible)
+                    nombreClienteLabel.Visibility = Visibility.Hidden;
+                else
+                    nombreClienteLabel.Visibility = Visibility.Visible;
+               
+        }
+
+        private void ApellidoTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+                if (apellidoClienteLabel.Visibility == Visibility.Visible)
+                    apellidoClienteLabel.Visibility = Visibility.Hidden;
+                else
+                    apellidoClienteLabel.Visibility = Visibility.Visible;
+
+        }
+
+        private void EdadTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F2)
+            {
+                bool sonNumeros = true;
+              
+                char[] c = edadTextBox.Text.ToCharArray();
+
+                for (int i = 0; i < c.Length; i++)
+                {
+                    if (c[i] < 48 || c[i] > 57)
+                            sonNumeros = false;
+                }
+
+                switch (sonNumeros)
+                {
+                    case true :
+                        edadIncorrectaLabel.Visibility = Visibility.Hidden;
+                        break;
+                    default:
+                        edadIncorrectaLabel.Visibility = Visibility.Visible;
+                        break;
+                }
+
+                
+            }
+        }
+    }
+}
